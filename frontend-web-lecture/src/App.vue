fe630bc3f757ede6645ea5729fc203d9f68b7084
richahoipen <minhyemen@gmail.com>
2025-01-14 17:33:58 +0700

    Changed name and create 2 project, FE lecture, student.

---
+++ frontend-web-lecture/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  border: solid 1px red;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+main{
+  border: solid 1px blue;
+}
+/*Giải thích: điều kiện khi chiều dài nhỏ nhất là 1024 thì sẽ áp dụng thuộc tính css dưới*/
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>

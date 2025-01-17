1023d94890cdaf3bc824098f83ed708614ad8e69
richahoipen <minhyemen@gmail.com>
2025-01-17 18:33:00 +0700

    Changed main.css

---
+++ backend-service/src/main/java/com/example/backend_service/securities/CorsConfigurations.java
@@ -0,0 +1,29 @@
+package com.example.backend_service.securities;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfigurations implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173", //vue
+                        "http://localhost:5174",
+                        "http://localhost:3000",  // ReactJS
+                        "http://localhost:8081",
+                        // React Native may minh
+                        // dùng http://localhost:8082
+                        "http://localhost:4200",  // Angular
+                        "http://localhost:3001" ,//Next.js
+                        "http://127.0.0.1:5173/",//
+                        "http://127.0.0.1:5174/"//
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                .allowedHeaders("*");
+    }
+}

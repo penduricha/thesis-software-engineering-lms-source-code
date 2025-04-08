da90689ab6b115a0dec1be0232041b1c413d9af6
richahoipen <minhyemen@gmail.com>
2025-04-08 09:13:11 +0700

    done create api bank test java oop.

--- frontend-web-student/src/components/aside/ModalViewDetailAccount.vue
+++ frontend-web-student/src/components/aside/ModalViewDetailAccount.vue
@@ -1,8 +1,9 @@
 <script>
 import StudentLocalStorage from "@/pages/login/StudentLocalStorage.js";
+import StudentDao from "@/daos/StudentDao.js";
 
 export default {
-  name: "ModalViewDetail_Information",
+  name: "ModalViewDetailAccount",
 
   data() {
     return {
@@ -21,25 +22,87 @@ export default {
       dateOfBirth: null,
       className: null,
       courseName: null,
+      gender: null,
     }
   },
 
   methods: {
-    async formatDateOFBirth(dateOfBirth) {
+    formatDateOFBirth(dateOfBirth) {
+      if(dateOfBirth) {
+        let dateOfBirthDate = new Date(dateOfBirth);
+        let day = dateOfBirthDate.getDate();
+        let month = dateOfBirthDate.getMonth() + 1;
+        let year = dateOfBirthDate.getFullYear();
+        return `${day}/${month}/${year}`;
+      }
+      return null;
+    },
 
+    formatName(firstName, lastName) {
+      if(firstName && lastName) {
+        return `${lastName} ${firstName}`;
+      }
+      return null;
+    },
+
+    getGender(gender) {
+      if(gender) {
+        return gender ? "Male" : "Female";
+      }
     },
 
     async setInformationStudent_By_StudentID() {
       const studentLocalStorage = new StudentLocalStorage();
       //console.log(student);
       this.studentID = studentLocalStorage.getStudentID_From_LocalStorage();
+      if(this.studentID) {
+        let detailInformation = await StudentDao.get_Detail_Information_Student_By_StudentID(this.studentID);
+        console.log("Detail Information", detailInformation);
+        if(detailInformation) {
+          this.lastName = detailInformation.lastName;
+          this.firstName =  detailInformation.firstName;
+          this.dateOfBirth = detailInformation.dateOfBirth;
+          this.className = detailInformation.className
+          this.courseName = detailInformation.courseName;
+          this.gender = detailInformation.gender;
+        }
+      }
     }
   }
 }
 </script>
 
 <template>
-
+  <div
+      class="modal fade"
+      id="modal-view-detail-account"
+      tabindex="-1"
+      aria-hidden="true"
+  >
+    <div class="modal-dialog modal-md">
+      <div class="modal-content content-info-exam">
+        <div class="modal-header">
+          <h5 class="modal-title">
+            Detail Information
+          </h5>
+          <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body modal-exam-body">
+          <label><span>Student id: {{ studentID }}</span></label>
+          <label><span>Name: {{ formatName(firstName, lastName) }}</span></label>
+          <label><span>Gender: {{ getGender(gender) }}</span></label>
+          <label><span>Date of birth: {{ formatDateOFBirth(dateOfBirth) }}</span></label>
+          <label><span>Course name: {{ courseName }}</span></label>
+          <label><span>Class name: {{ className }}</span></label>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
